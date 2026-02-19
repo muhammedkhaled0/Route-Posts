d39bba2d6f9f4ada0e9305556b7c85158c65d4f3
muhammedkhaled0 <muhammedkhaled7882@gmail.com>
2026-02-19 16:00:05 +0200

    adding navebar

--- src/compnents/Navbar.tsx
+++ src/compnents/Navbar.tsx
@@ -1,5 +1,41 @@
-export default function Nabar() {
-  return (
-    <div>Nabar</div>
-  )
+'use client'
+import Image from "next/image";
+import logo from "@/src/app/icon.png"
+import HomeIcon from "./icons/HomeIcon";
+import Link from "next/link";
+import PersonIcon from "./icons/PersonIcon";
+import MessageIcon from "./icons/MessageIcon";
+import BarIcon from "./icons/BarIcon";
+import { usePathname } from "next/navigation";
+export default function Navbar() {
+  const pathName=usePathname()
+  return <nav className="bg-white shadow">
+    <div className="myContainer py-2 flex justify-between align-baseline">
+      <div>
+        <div className=" flex gap-x-3">
+          <Image src={logo} alt="Route Logo" width={40} height={40} className="size-10 rounded-xl"/>
+          <h1 className=" mt-1.5 font-extrabold text-xl hidden sm:block">Route Posts</h1>
+        </div>
+      </div>
+      <div className="flex gap-x-7 font-extrabold text-sm text-gray-600 border rounded-2xl pt-3 px-4  bg-[#f7faff] border-gray-300">
+          <Link href='/' className={(pathName=='/'?"active":"")+" flex gap-x-1.5 align-baseline"}>
+          <HomeIcon className='size-5'/>
+          <span className="hidden sm:inline">Feed</span>
+          </Link>
+          <Link href='/profile' className={(pathName=='/profile'?"active":"")+ " flex gap-x-1.5 align-baseline"}>
+          <PersonIcon className='size-5'/>
+          <span className="hidden sm:inline">Profile</span>
+          </Link>
+          <Link href='/notifications' className={(pathName=='/notifications'?"active":"")+ " flex gap-x-1.5 align-baseline"}>
+          <MessageIcon className='size-5'/>
+          <span className="hidden sm:inline">Notifications</span>
+          </Link>
+      </div>
+      <div className="p-2 flex align-baseline gap-x-2 border rounded-2xl bg-[#f7faff] border-gray-300">
+          <Image src={logo} width={40} height={40} alt='User Photo' className="size-7 rounded-full"/>
+          <span className="text-sm font-extrabold text-gray-800 mt-1 hidden md:inline">Muhammed</span>
+          <BarIcon className='size-3 mt-2'/>
+      </div>
+    </div>
+  </nav>
 }
--- src/compnents/icons/PersonIcon.tsx
+++ src/compnents/icons/PersonIcon.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function PersonIcon(props:any) {
   return <>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"   {...props}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-</svg>
+  <svg xmlns="http://www.w3.org/2000/svg" {...props} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
 </>
 }
